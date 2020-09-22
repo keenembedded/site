28e61649ca9ac433d99033cc6df020b473f3ed02
siddheshJungade <siddheshjungade@gmail.com>
2020-09-22 13:58:06 +0530

    update seating

--- keenEmbedded/settings.py
+++ keenEmbedded/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'keenembedded.herokuapp.com ',
-]
+ALLOWED_HOSTS = ['keenembedded.herokuapp.com']
 
 
 # Application definition
