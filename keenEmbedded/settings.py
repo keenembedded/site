a4c50cc498e7405d596fd864aa78004bd08b4025
siddheshJungade <siddheshjungade@gmail.com>
2020-09-22 20:43:19 +0530

    cahnge in product.css

--- keenEmbedded/settings.py
+++ keenEmbedded/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'q-c%9x)%=1s-egk*#k)ukd&l=f0kyem!^ctwzefv=z2z+mp=y1'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'q-c%9x)%=1s-egk*#k)ukd&l=f0kyem!^ctwzefv=z2z+mp=y1'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['keenembedded.herokuapp.com']
+ALLOWED_HOSTS = ['keenembedded.herokuapp.com','127.0.0.1']
 
 
 # Application definition
